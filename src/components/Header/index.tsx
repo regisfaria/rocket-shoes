07ec42b4c058587db8f33f94fe138830db9247d4
regisfaria <rgisfaria@yahoo.com.br>
2021-07-07 00:07:24 -0300

    finished header cart info

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -7,8 +7,13 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  
+  const [cartSize, setCartSize] = useState(0);
+
+  useEffect(() => {
+    setCartSize(cart.length);
+  }, [cart]);
 
   return (
     <Container>
@@ -20,7 +25,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
